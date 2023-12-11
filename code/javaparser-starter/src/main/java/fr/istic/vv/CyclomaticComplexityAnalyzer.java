7d5ab38a1b5feb8434a2b74d485ff6dddff4ea4b
Viaud Benjamin <benjamin.viaud@etudiant.univ-rennes1.fr>
2023-12-11 10:22:03 +0100

    add histogram

--- code/javaparser-starter/src/main/java/fr/istic/vv/CyclomaticComplexityAnalyzer.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/CyclomaticComplexityAnalyzer.java
@@ -6,7 +6,9 @@ import com.github.javaparser.utils.SourceRoot;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.github.javaparser.ast.stmt.Statement;
@@ -23,6 +25,7 @@ public class CyclomaticComplexityAnalyzer {
         File projectPath = new File(args[0]);
 
         SourceRoot sourceRoot = new SourceRoot(projectPath.toPath());
+        Map<Integer, Integer> complexityHistogram = new HashMap<>();
 
         System.out.println(sourceRoot.tryToParse());
 
@@ -40,13 +43,19 @@ public class CyclomaticComplexityAnalyzer {
         for (MethodDeclaration method : methods) {
             int complexity = calculateCyclomaticComplexity(method);
             System.out.println("Method: " + method.getName() + ", Cyclomatic Complexity: " + complexity);
+            complexityHistogram.put(complexity, complexityHistogram.getOrDefault(complexity, 0) + 1);
+        }
+
+        System.out.println("Cyclomatic Complexity Histogram:");
+        for (Map.Entry<Integer, Integer> entry : complexityHistogram.entrySet()) {
+            System.out.println("CC: " + entry.getKey() + ", Occurrences: " + entry.getValue());
         }
     }
 
     private static int calculateCyclomaticComplexity(MethodDeclaration method) {
         List<Class<? extends com.github.javaparser.ast.Node>> controlFlowStatements = Arrays.asList(
                 IfStmt.class, SwitchStmt.class, WhileStmt.class, ForStmt.class, SwitchEntry.class,
-                TryStmt.class, ContinueStmt.class, BreakStmt.class
+                TryStmt.class
         );
 
         int controlFlowEdges = method.findAll(Statement.class)
